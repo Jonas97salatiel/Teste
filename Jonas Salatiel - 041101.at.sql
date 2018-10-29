545be6711633f9bb8c15d93de158ce7b28faef3f
Jonas97salatiel <38478074+Jonas97salatiel@users.noreply.github.com>
2018-10-28 23:49:07 -0300

    Add files via upload

---
+++ Jonas Salatiel - 041101.at.sql
@@ -0,0 +1,42 @@
+﻿CREATE DATABASE pratica05
+  WITH ENCODING='UTF8'
+   		 CONNECTION LIMIT=-1;
+
+select current_time;
+
+select sqrt(49);
+
+select current_schema;
+select current_database();
+
+select current_user;
+
+
+create sequence minha_seq increment by 10 minvalue 1 maxvalue 100 NO CYCLE; 
+
+select nextval('minha_seq');
+select setval('minha_seq',50);
+select currval('minha_seq');
+select nextval('minha_seq');
+ALTER SEQUENCE minha_seq INCREMENT BY 1;
+
+alter sequence minha_seq cycle;
+
+create table dados_multimidia(
+codigo SERIAL,
+nome varchar,
+tipo varchar,
+oid_dados oid,
+primary key(codigo));
+
+
+insert into(nome, tipo, oid_dados)values('imo', 'png', lo_import('C:\Users\Jonas\Desktop\IMO interface\Asset1.png'));
+
+INSERT INTO imagem (nome_imagem, oid_imagem)
+VALUES (‘Asset1’, lo_import(‘C:\Users\Jonas\Desktop\IMO interface\Asset1.png’));
+
+
+SELECT lo_export(oid_dados,
+'C:\Users\Jonas\Desktop\IMO interface\Asset1.png')
+FROM imagem
+WHERE nome = 'imo';
